c08697a90c102464b1dba36453f36620d3bcc70e
DelvinBerry <delvin@usca.edu>
2022-10-10 13:14:31 -0400

    done

---
+++ js/arith.js
@@ -0,0 +1,12 @@
+//get elements--get the text
+let spanE = document.getElementById('num1');
+
+let spanT = spanE.textContent;
+
+let n1 = parseInt(spanT);
+
+let n2 = parseInt(document.getElementById('num2').textContent);
+//add numbers
+let c = n1 + n2 ;
+// display
+document.write(c);
