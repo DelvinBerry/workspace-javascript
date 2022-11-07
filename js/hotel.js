29759ce9bfe43aa679c440b5996f56fe5a420b74
DelvinBerry <delvin@usca.edu>
2022-11-07 13:12:00 -0500

    Work

--- js/hotel.js
+++ js/hotel.js
@@ -15,9 +15,24 @@ $(function(){
     $('#submit').click(reservation);
 });
 
+
+var guestgroup = new Array();
 function reservation(){
     var guest = {
         name:('#name').val(),
-        numberofGuest: $('#num').val()
+        numberofGuest: $('#num').val(),
+        checkin: $('#checkin').val()
+    }
+    guestgroup.push(guest);
+    console.log(guestgroup);
+}
+
+
+function displayR(){
+    $("form").append("<table>");
+    for(var i in guestgroup){
+        var theguest = guestgroup(i);
+        var row = "<tr><td>"+theguest.name+"</td><td>"+theguest.checkin+"</td></tr>"
+        $("table").append(row);
     }
 }
